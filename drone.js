d828dd47d370e6f1e7b835ed8485e7574032ea94
StapleCactus764 <105686655+StapleCactus764@users.noreply.github.com>
2022-05-26 07:59:50 -0700

    Updating to current
    Added tracking, types, and a few other things.

--- drone.js
+++ drone.js
@@ -1,38 +1,47 @@
-const separation = 0.005,
+const separation = 0.010,
     cohesion = 0.025,
     alignment = 0.15,
-    sepSight = 20,
+    sepSight = 30,
     alignSight = 50,
-    cohesionSight = 500;
+    cohesionSight = Infinity;
 
 class Drone {
-    constructor() {
+    constructor(id = 'white') {
         this.pos = vec(Math.random()*50+200, Math.random()*50+200);
         this.velocity = vec(0, 0);
         this.acceleration = vec.norm(vec(Math.random()*2-1, Math.random()*2-1));
+        
+        this.id = id;
+        this.active = true;
+        this.maxVelocity = 1;
+        this.target = null;
+        this.podId = 0;
     }
     update() {
         this.pos = vec.add(this.pos, this.velocity);
         this.velocity = vec.add(this.velocity, this.acceleration);
-        vec.limit(this.velocity, 4);
+        vec.limit(this.velocity, this.maxVelocity);
         
         let sepDir = vec(0, 0);
         let center = this.pos,
             close = 0;
         let angle = vec(0, 0);
-        // console.log(center);
+        
         for (const drone of drones) {
             const dist = Math.hypot(this.pos.x-drone.pos.x, this.pos.y-drone.pos.y);
-            if (drone === this) continue;
+            if (!drone.active || drone === this) continue;
             
-            const scale = 1-dist/50+0.1;
+            const scale = 1-dist/30+0.1;
             
             if (dist < sepSight) {
                 sepDir = vec.add(
                     sepDir,
-                    vec.sub(this.pos, drone.pos)
+                    vec.mult(vec.sub(this.pos, drone.pos), scale)
                 );
             }
+            
+            if (drone.id !== this.id) continue;
+            
             if (dist < cohesionSight) {
                 center = vec.add(
                     center,
@@ -43,20 +52,23 @@ class Drone {
             close ++;
         }
         
-        if (sepDir.x && sepDir.y) sepDir = vec.mult(sepDir, separation);
-        
+        // Cohesion
+        center = vec.div(center, close);
+        const dirToCenter = vec.sub(center, this.pos);
         if (center.x && center.y && close) {
-            center = vec.div(center, close);
-            const dirToCenter = vec.norm(vec.sub(center, this.pos));
             
             this.acceleration = vec.norm(
-                vec.add(this.acceleration, vec.mult(dirToCenter, cohesion))
+                vec.add(this.acceleration, vec.mult(vec.norm(dirToCenter), cohesion))
             );
         }
+        
+        // Separation
+        if (sepDir.x && sepDir.y) sepDir = vec.mult(sepDir, separation);
         this.acceleration = vec.norm(
             vec.add(this.acceleration, sepDir)
         );
         
+        // Alignment
         if (close) {
             const currentAcc = vec.copy(this.acceleration);
             this.acceleration = vec.norm(
@@ -64,18 +76,44 @@ class Drone {
             );
         }
         
-        if (this === drone[0]) {
-            // ctx.beginPath();
-            // ctx.strokeStyle = 'green';
-            // ctx.lineWidth = 4;
-            // ctx.moveTo(this.pos.x, this.pos.y);
-            // ctx.lineTo(this.pos.x + angle.x*5, this.pos.y+angle.y*5);
-            // ctx.stroke();
-            // ctx.lineWidth = 1;
-            // ctx.closePath();
+        // Targeting
+        if (pods[this.podId]?.target) {
+            const dirToTarget = vec.sub(
+                vec(pods[this.podId].target.pos.x, pods[this.podId].target.pos.y),
+                this.pos
+            );
+            
+            this.acceleration = vec.norm(vec.add(
+                this.acceleration,
+                vec.mult(vec.norm(dirToTarget), 0.1)
+            ));
+            
+            
+            if (Math.hypot(dirToTarget.x, dirToTarget.y) < pods[this.podId].target.radius) {
+                this.active = false;
+            }
+        }
+        
+        // Obstacles
+        for (const obj of gravityObj) {
+            if (obj === pods[this.podId]?.target) continue;
+            const dist = Math.hypot(obj.pos.x-this.pos.x, obj.pos.y-this.pos.y);
+            if (dist < 80 * obj.radius/30) {
+            
+                const scale = 0.5-(dist-30)/100+0;
+                const dirToObs = vec.sub(this.pos, obj.pos)
+                
+                this.acceleration = vec.norm(vec.add(
+                    this.acceleration,
+                    vec.mult(vec.norm(dirToObs), scale)
+                ));
+            }
         }
         
         
+        
+        
+        // Boundaries
         if (this.pos.x < 50) {
             this.acceleration = vec.norm(
                 vec.add(
@@ -110,15 +148,19 @@ class Drone {
         }
     }
     display() {
-        ctx.fillStyle = ctx.strokeStyle = 'white';
+        ctx.fillStyle = ctx.strokeStyle = this.id;
         ctx.beginPath();
-        ctx.arc(this.pos.x, this.pos.y, 3, 0, tau);
+        ctx.arc(this.pos.x, this.pos.y, 2, 0, tau);
         ctx.closePath();
         ctx.fill();
         ctx.beginPath();
         ctx.moveTo(this.pos.x, this.pos.y);
-        ctx.lineTo(this.pos.x + this.velocity.x*2, this.pos.y + this.velocity.y*2);
+        ctx.lineTo(this.pos.x + this.velocity.x*4, this.pos.y + this.velocity.y*4);
         ctx.closePath();
         ctx.stroke();
+    }
+    
+    static createTypeFromId(id) {
+        return ids[id];
     }
 }
